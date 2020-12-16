8f0f1fa25a6964208b4cb67532f8deecf5feaf48
msaqibjavaid <45518350+msaqibjavaid@users.noreply.github.com>
2020-12-16 01:38:41 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,87 @@
 <?php include("header.php");?>
+
+<div class="slider-sec" style="background: url(assets/images/slider-background.jpg) no-repeat scroll center top;">
+	<div id="slider" class="flexslider">
+		<ul class="slides">
+		  <li style="background: url(uploads/slider/sl-1.jpg) no-repeat scroll center top;">
+				<h2>Lot's of Courses and Thousand of Exams</h2>
+				<p>Biuld your career on Quiz and Exam Portal with hudred of courses and thousand of exams. Make score and pass get a certificate of demonstration of your knowledge.</p>
+				<a href="courses.php" class="button button-red ">Learn More</a>
+		  </li>
+		  <li style="background: url(uploads/slider/sl-2.jpg) no-repeat scroll center top;">
+				<h2>Online Certification and Skill Testing for Individuals</h2>
+				<p>Quiz and Exam Portal is a leader in Online Certification and Skill Testing and offers an affordable and efficient way for people to prove their expertise.</p>
+				<a href="exams.php" class="button button-red ">Learn More</a>
+		  </li>
+		 
+		</ul>
+	</div>
+</div>
+
+
+<div class="headline">
+	<h1>Boost Your Career With Online Quiz and Exam Portal</h1>
+</div>
+
+<div class="features clearfix">
+    <div class="col grid_8">
+		<p><i class="fa fa-camera-retro feature-icon"></i></p>
+		<h3>More Courses & Exam</h3>
+	
+    </div>
+    <div class="col grid_8">
+		<p><i class="fa fa-camera-retro feature-icon"></i></p>
+		<h3>Online Certification</h3>
+		
+    </div>
+    <div class="col grid_8">
+		<p><i class="fa fa-camera-retro feature-icon"></i></p>
+		<h3>Boost Your Career</h3>
+		
+    </div>
+</div>
+
+<div class="call-to-action clearfix">
+	<h1>Get started today with a free course program!</h1>
+	<ul class="list-separator">
+	  <li>Get Certified <span class="separator">|</span></li>
+	  <li> Explore Marks <span class="separator">|</span> </li>
+	  <li> Explore Success</li>
+	</ul>
+	<a href="register.php" class="button button-green">Register Now!</a>
+</div> 
+
+
+
+<div  class="box" id="loginbox">
+  <div id="msgholder2"><!-- will show alert message here later --></div>
+  <form method="post" id="login_form" name="login_form" class="xform">
+    
+	<header>Login to your user account</header>
+	
+    <section>
+      <div class="row">
+        <div class="col col-3">
+          <label>Username</label>
+        </div>
+        <div class="col col-9">
+          <label class="input"> <i class="icon-prepend icon-user"></i> <i class="icon-append icon-asterisk"></i>
+            <input  type="text" name="username" placeholder="Enter Your Username">
+          </label>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="row">
+        <div class="col col-3">
+          <label>Password</label>
+        </div>
+        <div class="col col-9">
+          <label class="input"> <i class="icon-prepend icon-lock"></i> <i class="icon-append icon-asterisk"></i>
+            <input type="password"  name="password" placeholder="**********">
+          </label>
+        </div>
+      </div>
+    </section>
+
+<?php include("footer.php");?>    
