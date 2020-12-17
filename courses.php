863d0b5c58d752311da94f9c81e91e528e271d45
msaqibjavaid <45518350+msaqibjavaid@users.noreply.github.com>
2020-12-16 23:02:51 -0800

    Create courses.php
    added courses.php file for front end static for now

---
+++ courses.php
@@ -0,0 +1,56 @@
+<?php include("header.php");?>
+<div class="row xform course-box">
+	<h1>Available Courses</h1>
+		<section class="col col-4">
+	  <div class="box">
+		  <div class="course-upper">
+			<a href="#"><h1>Demo Course 1</h1></a>
+			<h4>5 Exams</h4>
+			<h2>FREE</h2>
+			
+		  </div>
+		  <div class="course-lower">
+			<a href="#" class="button course-details">Details</a>
+				
+					<a href="account.php" class="button ">Enrol Now</a>
+				
+		  </div>
+	  </div>
+	</section>
+	<section class="col col-4">
+	  <div class="box">
+		  <div class="course-upper">
+			<a href="#"><h1>Demo Course 2</h1></a>
+			<h4>3 Exams</h4>
+			<h2>5 USD</h2>
+			
+		  </div>
+		  <div class="course-lower">
+			<a href="#" class="button course-details">Details</a>
+				
+					<a href="account.php" class="button ">Enrol Now</a>
+				
+		  </div>
+	  </div>
+	</section>
+
+	<section class="col col-4">
+	  <div class="box">
+		  <div class="course-upper">
+			<a href="#"><h1>Demo Course 3</h1></a>
+			<h4>2 Exams</h4>
+			<h2>1 USD</h2>
+			
+		  </div>
+		  <div class="course-lower">
+			<a href="#" class="button course-details">Details</a>
+				
+					<a href="account.php" class="button ">Enrol Now</a>
+				
+		  </div>
+	  </div>
+	</section>
+	
+</div>
+
+<?php include("footer.php");?>
