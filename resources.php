2996d74f85399b2e6b01162ebf34c5315ae98a97
msaqibjavaid <45518350+msaqibjavaid@users.noreply.github.com>
2020-12-17 22:30:00 -0800

    Create resources.php

---
+++ resources.php
@@ -0,0 +1,29 @@
+<?php include("header.php");?>
+<div class="row xform">
+        <section class="col col-9">
+			
+			<a href="resources.php"><h1>Example Resource 1</h1></a>
+			<h4>Demo Category 1</h4>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
+			
+		</section>
+		<section class="col col-3" style="border-left: 1px solid #e3e3e3;">
+			
+			
+			<a href="resources.php"><h3>Demo Category 1</h3></a>
+			<a href="resources.php"><h3>Demo Category 2</h3></a>
+			
+		</section>
+	</div>
+
+    <div class="row xform">
+        <section class="col col-9">
+			
+			<a href="resources.php"><h1>Example Resource 2</h1></a>
+			<h4>Demo Category 2</h4>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
+			
+		</section>
+		
+	</div>
+<?php include("footer.php");?>
