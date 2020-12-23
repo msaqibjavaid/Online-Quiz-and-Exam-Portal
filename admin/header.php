83c8cf2820ad9367bcb8468c58e502b050076098
msaqibjavaid <45518350+msaqibjavaid@users.noreply.github.com>
2020-12-22 23:11:29 -0800

    Create header.php
    added header.php file included necessary css and js files including bootstrap and jquery files

---
+++ admin/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Online Quiz and Exam portal</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link rel="stylesheet" href="../theme/master/admin/style/style.css"  type="text/css" />
+<link rel="stylesheet" href="../assets/style/jquery-ui.css" type="text/css" />
+<script src="../assets/js/jquery.js"></script>
+<script src="../assets/js/jquery-ui.js"></script>
+<script src="../assets/js/jquery.ui.touch-punch.js"></script>
+<script src="../assets/js/jquery.wysiwyg.js"></script>
+<script src="../assets/js/global.js"></script>
+<script src="../assets/js/custom.js"></script>
+<script src="../assets/js/nicescroll.js" type="text/javascript" ></script>
+<script src="../assets/js/modernizr.mq.js" type="text/javascript" ></script>
+<script src="../assets/js/checkbox.js"></script>
+<script src="../assets/fancybox/jquery.fancybox.pack.js"></script>
+<script src="../assets/fancybox/helpers/jquery.fancybox-media.js"></script>
+<link rel="stylesheet" type="text/css" href="../assets/fancybox/jquery.fancybox.css" media="screen" />
+<script src="admin/js/menu.js"></script>
+
+</head>
+<body data-smooth-scrolling="1">
+<div id="loader" style="display:none"></div>
+<div class="bg">
+  <div class="container">
+  
+<div class="row grid_24"  id="crumbs">
+	
+	<div class="col grid_5">
+		<div class="header-logo"><a href="index.php"><img src="../uploads/logo.png" alt="Quiz and Exam Portal" class="logo"/>Quiz and Exam portal</a></div>
+	</div>
+	<div class="col grid_19">
+		<section class="row">
+		  <div class="col grid_16">
+		
+		  </div>
+		 
