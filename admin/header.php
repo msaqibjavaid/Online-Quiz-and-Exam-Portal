85cbd7769e7cb96d1374bddaafb472e9fc2207d3
msaqibjavaid <45518350+msaqibjavaid@users.noreply.github.com>
2020-12-23 20:46:50 -0800

    Update header.php
    added sidebar and header for admin pages and done some styling in js and css folders

--- admin/header.php
+++ admin/header.php
@@ -36,4 +36,61 @@
 		  <div class="col grid_16">
 		
 		  </div>
-		 
+ <div class="col grid_8"> 
+			<!-- User Panel -->
+			<div id="userpanel"> <img src="../uploads/avatars/avator.png";?>Admin Image<span>Admin</span>
+			  <ul class="profilenav">
+				<li> <a href="#" id="showhide">
+				  <p>English</p>
+				  </a>
+				  <div id="settingslist">
+					<ul class="sub">
+					  <li><i class="icon-reorder pull-left"></i> <a href="index.php">Account Settings</li>
+					  <li><i class="icon-signout pull-left"></i> <a href="logout.php">Logout</a></li>
+					</ul>
+				  </div>
+				</li>
+			  </ul>
+			</div>
+			<!-- User Panel /-->  
+		  </div>
+		</section>
+	</div>
+</div>
+	
+<div class="row grid_24 clearfix">
+	<div id="bitset" class="col grid_5">
+	<div id="truecontent">
+		<!-- Header -->
+		<header id="header" class="clearfix">
+			<nav class="cbp-hsmenu-wrapper" id="cbp-hsmenu-wrapper">
+			  <div class="cbp-hsinner">
+				<ul class="cbp-hsmenu">
+				  
+				  <li> <a href="#">User Manager</a>
+					<ul class="cbp-hssubmenu">
+					  <li><a href="index.php"><i class="icon-user menu-icon"></i><span>Staff</span></a></li>
+					  
+					  <li><a href="index.php"><i class="icon-users menu-icon"></i><span>Clients</span></a></li>
+					  <?php endif;?>
+					</ul>
+				  </li>
+				  <li> <a href="#">Course & Exams</a>
+					<ul class="cbp-hssubmenu">
+					  
+					  <li><a href="course.php"><i class="icon-copy menu-icon"></i><span>Manage Courses</span></a></li>
+					  <li><a href="exam.php"><i class="icon-exams menu-icon"></i><span>Manage Exams</span></a></li>
+					  <li><a href="question.php"><i class="icon-ques menu-icon"></i><span>Manage Questions</span></a></li>
+					  <li><a href="addquestion.php"><i class="icon-addq menu-icon"></i><span>Add Question</span></a></li>					  
+					  <?php endif;?>
+					  <li><a href="result.php"><i class="icon-result menu-icon"></i><span>Results</span></a></li>
+					</ul>
+				  </li>
+				  <li> <a href="#">Enrolment & Billing</a>
+					<ul class="cbp-hssubmenu">
+					  <li><a href="enroll.php"><i class="icon-enrol menu-icon"></i><span>Enrolment</span></a></li>
+					  <li><a href="transactions.php"><i class="icon-trans menu-icon"></i><span>Transactions</span></a></li>
+					  <li><a href="gateways.php"><i class="icon-payment menu-icon">Gateways</span></a></li>
+					</ul>
+				  </li>
+				 
