e3fefd2cc3ab4d0848d48b5afc23e5ff784e4b00
msaqibjavaid <45518350+msaqibjavaid@users.noreply.github.com>
2020-12-24 22:11:15 -0800

    Update index.php
    completed static dashboard for admin with beautiful cards and a table

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,101 @@
 <?php include("header.php");?>
+<script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
+  <div class="row" style="margin: 8px;">
+
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>5</h3>
+
+                <p>Active Users</p>
+              </div>
+              <div class="icon">
+                <span class="iconify" data-icon="ant-design:user-outlined" data-inline="false"></span>
+              </div>
+              <a href="users.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>3</h3>
+
+                <p>Courses</p>
+              </div>
+              <div class="icon">
+               <span class="iconify" data-icon="bx:bxs-book" data-inline="false"></span>
+              </div>
+              <a href="courses.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <h3>44</h3>
+
+                <p>Staff</p>
+              </div>
+              <div class="icon">
+                <span class="iconify" data-icon="fa-solid:users" data-inline="false"></span>
+              </div>
+              <a href="staff.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6" >
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>65</h3>
+
+                <p>Active Exams</p>
+              </div>
+              <div class="icon">
+               <span class="iconify" data-icon="raphael:paper" data-inline="false"></span>
+              </div>
+              <a href="exams.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+        </div>
+        <div class="card" style="margin: 8px;">
+              <div class="card-header">
+                <h1 class="card-title">Recent Enrolments</h1>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <table id="example2" class="table table-bordered table-hover">
+                  <thead>
+                  <tr>
+                    <th>Username</th>
+                    <th>Course Title</th>
+                    <th>Date</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                 
+                 
+                  <tr>
+                    <td>Noman Aslam</td>
+                    <td>Final Year Project</td>
+                    <td>24 December 2020</td>
+                  </tr>
+                  <tr>
+                    <td>Saqib Javaid</td>
+                    <td>Final Year Project</td>
+                    <td>24 December 2020</td>
+                  </tr>
+                  </tbody>
+                 
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
 </div>
+
 <?php include("footer.php");?>
