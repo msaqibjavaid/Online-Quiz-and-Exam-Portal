cd4bbbdc8fb8895e8c4809eb64f599c5346e4756
msaqibjavaid <45518350+msaqibjavaid@users.noreply.github.com>
2021-01-04 02:46:46 -0800

    Update addstaff.php
    updated add staff form for mobile and other devices also added all necessary fields

--- admin/addstaff.php
+++ admin/addstaff.php
@@ -1,65 +1,111 @@
 <?php include("header.php");?>
-<div class="container" style="margin: 4%">
-    <h1 class="well">Add Staff</h1>
-  <div class="col-lg-12 well">
+<div class="container" >
+  <form class="xform" id="admin_form" method="post">
+     <header>Add Staff<span>Fill in the Details</span></header>
+    <div class="row">
+     <section class="col col-6">
+      <label class="input"> <i class="icon-prepend icon-user"></i> <i class="icon-append icon-asterisk"></i>
+        <input type="text" name="username" placeholder="Username">
+      </label>
+      <div class="note note-error">This is a required field.</div>
+    </section>
+    <section class="col col-6">
+      <label class="input"> <i class="icon-prepend icon-lock"></i> <i class="icon-append icon-asterisk"></i>
+        <input type="text" name="password" placeholder="Password">
+      </label>
+      <div class="note note-error">This is a required field.</div>
+    </section>
+  </div>
+   <div class="row">
+    <section class="col col-6">
+      <label class="input"> <i class="icon-prepend icon-envelope-alt"></i> <i class="icon-append icon-asterisk"></i>
+        <input type="text" name="email" placeholder="Email Address">
+      </label>
+      <div class="note note-error">This is a required field.</div>
+    </section>
+    <section class="col col-6">
+      <label class="input"> <i class="icon-prepend icon-phone"></i>
+        <input type="text" name="phone" placeholder="Phone Number">
+      </label>
+    </section>
+  </div>
   <div class="row">
-        <form class="xform" id="admin_form" method="post">
-          <div class="col-sm-12">
-            <div class="row">
-              <div class="col-sm-6 form-group">
-                <label>First Name</label>
-                <input type="text" placeholder="Enter First Name Here.." class="form-control">
-              </div>
-              <div class="col-sm-6 form-group">
-                <label>Last Name</label>
-                <input type="text" placeholder="Enter Last Name Here.." class="form-control">
-              </div>
-            </div>          
-            <div class="form-group">
-              <label>Address</label>
-              <textarea placeholder="Enter Address Here.." rows="3" class="form-control"></textarea>
-            </div>  
-            <div class="row">
-              <div class="col-sm-4 form-group">
-                <label>City</label>
-                <input type="text" placeholder="Enter City Name Here.." class="form-control">
-              </div>  
-              <div class="col-sm-4 form-group">
-                <label>State</label>
-                <input type="text" placeholder="Enter State Name Here.." class="form-control">
-              </div>  
-              <div class="col-sm-4 form-group">
-                <label>Zip</label>
-                <input type="text" placeholder="Enter Zip Code Here.." class="form-control">
-              </div>    
-            </div>
-            <div class="row">
-              <div class="col-sm-6 form-group">
-                <label>Title</label>
-                <input type="text" placeholder="Enter Designation Here.." class="form-control">
-              </div>    
-              <div class="col-sm-6 form-group">
-                <label>Company</label>
-                <input type="text" placeholder="Enter Company Name Here.." class="form-control">
-              </div>  
-            </div>            
-          <div class="form-group">
-            <label>Phone Number</label>
-            <input type="text" placeholder="Enter Phone Number Here.." class="form-control">
-          </div>    
-          <div class="form-group">
-            <label>Email Address</label>
-            <input type="text" placeholder="Enter Email Address Here.." class="form-control">
-          </div>  
-          <div class="form-group">
-            <label>Website</label>
-            <input type="text" placeholder="Enter Website Name Here.." class="form-control">
-          </div>
-          <button type="button" class="btn btn-lg btn-info">Submit</button>         
-          </div>
-        </form> 
-        </div>
+    <section class="col col-6">
+      <label class="input"> <i class="icon-prepend icon-user"></i> <i class="icon-append icon-asterisk"></i>
+        <input type="text" name="fname" placeholder="First Name">
+      </label>
+      <div class="note note-error">This is a required field.</div>
+    </section>
+    <section class="col col-6">
+      <label class="input"> <i class="icon-prepend icon-user"></i> <i class="icon-append icon-asterisk"></i>
+        <input type="text" name="lname" placeholder="Last Name">
+      </label>
+      <div class="note note-error">This is a required field.</div>
+    </section>
   </div>
+  <hr>
+   <div class="row">
+    <section class="col col-5" >
+      <select name="country" style="width:100%;height: 39px;background: #FBFBFB">
+        <option disabled="" selected="" value="0">&nbsp;Country</option>
+        
+        <option value="Pakistan">Pakistan</option>
+        
+      </select>
+    </section>
+    <section class="col col-4">
+      <label class="input"> <i class="icon-prepend icon-flag"></i>
+        <input type="text" name="city" placeholder="City">
+      </label>
+    </section>
+    <section class="col col-3">
+      <label class="input"> <i class="icon-prepend icon-pushpin"></i>
+        <input type="text" name="zip" placeholder="ZipCode">
+      </label>
+    </section>
   </div>
+   <div class="row">
+    <section class="col col-4">
+      <label class="input">
+        <input name="avatar" type="file" class="fileinput"/>
+      </label>
+      <div class="note note">Avator</div>
+    </section>
+  </div>
+    <div class="row">
+    <section class="col col-6">
+      <label class="input"> <i class="icon-prepend icon-map-marker"></i>
+        <input type="text" name="address" placeholder="Address">
+      </label>
+    </section>
+    <section class="col col-6">
+      <label class="input"> <i class="icon-prepend icon-umbrella"></i>
+        <input type="text" name="state" placeholder="State/Province">
+      </label>
+    </section>
+  </div>
+  <hr>
+   <header>Access Level</header>
+  <section>
+    <div>
+      <label class="radio">
+        <input type="radio" name="userlevel" value="9">
+        <i></i>Admin</label>
+      <label class="radio">
+        <input type="radio" name="userlevel" checked="checked" value="5">
+        <i></i>Staff</label>
+      <div class="note note-info">Admin has full rights. Staff has access to just add exam and to view results.</div>
+      <label class="checkbox">
+        <input type="checkbox" name="notify" value="1">
+        <i></i>Notify User</label>
+      <div class="note note-info">Send welcome email to this user.</div>
+    </div>
+  </section>
+  <footer > 
+  <button class="button" name="dosubmit" type="submit" style="align-content: left">Adrd Staff</button>
+    <a href="index.php" class="button button-secondary">Cancel</a> </footer>
 </div>
+ 
+</form>
+</div>  
 <?php include("footer.php");?>
