033f4539e7696cf8dca20165d4d8fb57b502a20a
msaqibjavaid <45518350+msaqibjavaid@users.noreply.github.com>
2021-01-14 01:01:58 -0800

    Create footer.php
    created footer file for staff  portal

---
+++ staff/footer.php
@@ -0,0 +1,29 @@
+
+   <footer class="main-footer">
+    <strong>Copyright &copy; 2020 <a href="https:saqibz.com">Final Year Project Comsats</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Vehari</b> Campus 
+    </div>
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="../admin/style/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="../admin/style/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+
+<script src="../admin/style/adminlte.js"></script>
+</body>
+</html>
