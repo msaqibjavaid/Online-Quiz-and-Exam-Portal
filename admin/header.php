0e27804f03984416ef9d1eea26dcdd3fc9cda0a2
msaqibjavaid <45518350+msaqibjavaid@users.noreply.github.com>
2021-01-14 00:52:31 -0800

    Update header.php
    updated logo for admin and website logo

--- admin/header.php
+++ admin/header.php
@@ -62,9 +62,9 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Exam Portal</span>
+    <a href="../index.php" class="brand-link">
+      <img src="../assets/images/logo.png" alt="Quiz and Exam Portal" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light"></span>
     </a>
 
     <!-- Sidebar -->
@@ -72,10 +72,10 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="../assets/images/admin.png" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Noman and Saqib</a>
+          <a href="" class="d-block">Noman and Saqib</a>
         </div>
       </div>
 
@@ -232,6 +232,37 @@
                 </a>
               </li>
         </ul>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-cog"></i>
+              <p>
+                System Setting
+                <i class="fas fa-angle-left right"></i>
+                             </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="config.php" class="nav-link">
+                  <i class="nav-icon fas fa-angle-right"></i>
+                  <p>System Configuration</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="backup.php" class="nav-link">
+                  <i class="nav-icon fas fa-angle-right"></i>
+                  <p>Database Backup</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="email.php" class="nav-link">
+                  <i class="nav-icon fas fa-angle-right"></i>
+                  <p>Email Settings</p>
+                </a>
+              </li>
+             
+            </ul>
+          </li>
+        
       </nav>
       <!-- /.sidebar-menu -->
     </div>
