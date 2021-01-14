25300c2a5117767bb0422b4b96d4d3ec0f57e682
msaqibjavaid <45518350+msaqibjavaid@users.noreply.github.com>
2021-01-14 01:07:20 -0800

    Create results.php
    created show results page for staff  portal

---
+++ staff/results.php
@@ -0,0 +1,48 @@
+<?php include("header.php");?>
+
+ <div class="card" style="margin: 8px;">
+              <div class="card-header">
+                <h1 class="card-title"><i class="icon-reorder"></i> &nbsp;Viewing Results</h1>
+                   
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <table id="example2" class="table table-bordered table-hover">
+                  <thead>
+                  <tr>
+                    <th>Username</th>
+                    <th>Exam Title</th>
+                    <th>Score</th>
+                    <th>Date</th>
+                    <th>Actions</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                 
+                 
+                  <tr>
+                    <td>Saqibjavaid</td>
+                    <td>Demo Exam 1</td>
+                     <td>90%</td>
+                  <td>2020-01-11</td>
+           <td>&nbsp;
+          <span class="tbicon"> <a href="delete" ><i class="icon-trash"></i></a> </span>
+         </td>
+                  </tr>
+                   <tr>
+                    <<td>nomanaslam</td>
+                    <td>Demo Exam 2</td>
+                     <td>90%</td>
+                  <td>2020-01-11</td>
+           <td>&nbsp;
+          <span class="tbicon"> <a href="delete" ><i class="icon-trash"></i></a> </span>
+         </td>
+                  </tr>
+                  </tbody>
+                 
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+</div>
+<?php include("footer.php");?>
