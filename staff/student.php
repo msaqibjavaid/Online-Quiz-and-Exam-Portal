5ed629f02d1326dd961144f12a6cc377191d45c6
msaqibjavaid <45518350+msaqibjavaid@users.noreply.github.com>
2021-01-14 01:05:26 -0800

    updated as student files as per permissions for staff

--- staff/student.php
+++ staff/student.php
@@ -2,7 +2,7 @@
 <div class="card" style="margin: 8px;">
               <div class="card-header">
                 <h1 class="card-title"><i class="icon-reorder"></i> &nbsp;Viewing Students</h1>
-                   <aside style="text-align: right"> <a href="addstudent.php"><span class="icon-plus">&nbsp;Add Students</span></a> </aside>
+                   
               </div>
               <!-- /.card-header -->
               <div class="card-body">
@@ -11,7 +11,7 @@
                   <tr>
                     <th>Name</th>
                     <th>Email Address</th>
-                    <th>Actions</th>
+                   
                   </tr>
                   </thead>
                   <tbody>
@@ -20,16 +20,14 @@
                   <tr>
                     <td>Noman Aslam</td>
                     <td>noman@gmail.com</td>
-           <td>&nbsp;<span class="tbicon"> <a href="editstudent.php" ><i class="icon-pencil"></i></a> </span>&nbsp;
-          <span class="tbicon"> <a href="delete" ><i class="icon-trash"></i></a> </span>
+         
          </td>
                   </tr>
                   <tr>
                     <td>Saqib Javaid</td>
                     <td>saqib@gmail.com</td>
                     
-           <td>&nbsp;<span class="tbicon"> <a href="editstudent.php" ><i class="icon-pencil"></i></a> </span>&nbsp;
-          <span class="tbicon"> <a href="delete" ><i class="icon-trash"></i></a> </span>
+         
          </td>
                   </tr>
                   </tbody>
