7a332bc813b6c68e24cd09e7e1f8692c3c8ed805
msaqibjavaid <45518350+msaqibjavaid@users.noreply.github.com>
2021-01-14 01:08:28 -0800

    Create examdetails.php
    updated exam details page

---
+++ examdetails.php
@@ -0,0 +1,17 @@
+<?php include("header.php"); ?>
+<div class="row">
+	<section class="xform">
+		<h1>Exam Name: Demo Exam 1</h1>
+		<h2>Total Question: 25</h2>
+		<h2>Duration: 25</h2><br/>
+		<p>Syllabus: Chapter 1 to Demo</p>
+		
+		
+			<a href="quize.php" class="button">Take This Exam</a>
+	
+		
+	</section>
+</div>
+
+
+<?php include("footer.php");?>
