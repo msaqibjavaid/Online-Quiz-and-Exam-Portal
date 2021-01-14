129be0e638f5ef70562689071ee8353d3af8a01b
msaqibjavaid <45518350+msaqibjavaid@users.noreply.github.com>
2021-01-14 01:09:38 -0800

    Create result.php
    result page that shows the result at the end of each exam

---
+++ result.php
@@ -0,0 +1,19 @@
+<?php include("header.php"); ?>
+<section class="widget">
+  <div class="result">
+	<h1>Exam Result</h1>
+	<h1>Test Name: Demo Exam 1</h1>
+	<h3>Full Marks: 25</h3>
+	<h3>You Scored: 24</h3>
+	
+	<h3>Percentage : 96 %</h3>
+	<h3>Passing Score : 50 %</h3><br><br>
+		<h2>Congratulation, You Passed!!!</h2><br>
+		
+		<div class="progress-bar ui-progressbar ui-widget ui-widget-content"><div class="inner-green ui-corner-left" style="width:90%;">96%&nbsp;&nbsp;</div></div>
+
+	<br><br><br><br>
+	<p><a href="exams.php" class="button button-secondary">Review Exam</a> <a href="courses.php" class="button button-secondary">Go back to courses</a></p><br><br>
+  </div>
+</section>
+<?php include("footer.php"); ?>
