6158e53a02a392ebcf8cf49a31fa8acfa42a016f
msaqibjavaid <45518350+msaqibjavaid@users.noreply.github.com>
2021-01-14 01:02:15 -0800

    Create index.php
    created index file for staff  portal

---
+++ staff/index.php
@@ -0,0 +1,100 @@
+<?php include("header.php");?>
+  <div class="row" style="margin: 8px;">
+
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>5</h3>
+
+                <p>Active Users</p>
+              </div>
+              <div class="icon">
+               <i class="fas fa-user"></i>
+              </div>
+              <a href="student.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>3</h3>
+
+                <p>Courses</p>
+              </div>
+              <div class="icon">
+               <i class="fas fa-book"></i>
+              </div>
+              <a href="courses.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <h3>44</h3>
+
+                <p>Questions</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-question"></i>
+              </div>
+              <a href="managequestions.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6" >
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>65</h3>
+
+                <p>Active Exams</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-scroll"></i>
+              </div>
+              <a href="exams.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+        </div>
+        <div class="card" style="margin: 8px;">
+              <div class="card-header">
+                <h1 class="card-title">Recent Enrolments</h1>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <table id="example2" class="table table-bordered table-hover">
+                  <thead>
+                  <tr>
+                    <th>Username</th>
+                    <th>Course Title</th>
+                    <th>Date</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                 
+                 
+                  <tr>
+                    <td>Noman Aslam</td>
+                    <td>Final Year Project</td>
+                    <td>24 December 2020</td>
+                  </tr>
+                  <tr>
+                    <td>Saqib Javaid</td>
+                    <td>Final Year Project</td>
+                    <td>24 December 2020</td>
+                  </tr>
+                  </tbody>
+                 
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+</div>
+
+<?php include("footer.php");?>
